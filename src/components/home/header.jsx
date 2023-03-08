0a12a2a294eb07426e87f8a6f344ffc763c9757f
Eliti2022 <eliti2022@pop-os.localdomain>
2023-03-08 17:18:58 -0300

    feat: add home page layout

---
+++ src/components/home/header.jsx
@@ -0,0 +1,11 @@
+import "../../styles/home/header.css"
+
+const Header = () => {
+    return(
+        <header id="home-header">
+            <h1>Meu Varejo</h1>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import "../styles/home/home.css"
+import Header from "../components/home/header.jsx"
+
+
+const Home = () => {
+    return(
+        <div id="home">
+            <Header />
+        </div>
+    )
+}
+export default Home
