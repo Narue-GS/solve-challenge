4b3062f9d752de19f8b96fca79a823f9c1809dfd
Naruê Gonçalves Santos <naruegit@gmail.com>
2023-03-13 16:52:06 -0300

    feat: add delete product feature;

--- src/components/home/productList.jsx
+++ src/components/home/productList.jsx
@@ -1,13 +1,21 @@
 import "../../styles/home/productList.css"
 
-const ProductList = ({products}) => {
+const ProductList = ({products, remove, mode}) => {
 	return(
 		<div id="car-product-list">
       { products.map((product) => {  
         return(
-					<div id="car-product">
-      			<span>{product.name}</span>
-      			<span>R${product.price}</span>
+					<div id="car-product" key={product.id}>
+						{mode==="edit"?
+						<div id="edit-product" onClick={() => remove(product)}>
+							<strong>X</strong>
+						</div> 
+						: <></>
+						}
+						<div id="product-detail">
+							<span>{product.name}</span>
+							<span>R${product.price}</span>
+						</div>
     			</div>
 				)
       })} 
